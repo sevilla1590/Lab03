98a431b3f241645f0e568a9d08f16fd037fe8a2e
sevilla1590 <sevillaizan@gmail.com>
2024-09-21 02:15:06 -0500

    Agregado manejo de lista de tareas

---
+++ agregar_tarea.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['tareas'])){
+    $_SESSION['tareas'] = [];
+}
+
+$tareas = $_SESSION['tareas'];
+
+$nueva_tarea - $_POST['tarea'];
+
+$tareas[] = $nueva_tarea;
+
+$_SESSION['tareas'] = $tareas;
+
+header('Location:index.php');
+?>
